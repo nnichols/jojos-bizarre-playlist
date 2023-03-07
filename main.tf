e68f755bf4fd77db43362e40617a99d9aa7f8ee5
Nick Nichols <nichols1991@gmail.com>
2023-03-07 00:46:26 +0000

    Add providers.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    spotify = {
-      version = "~> 0.2.7"
-      source  = "conradludgate/spotify"
-    }
-  }
-}
-
-provider "spotify" {
-  api_key = var.spotify_api_key
-}
-
 resource "spotify_playlist" "playlist" {
   name        = "JoJo's Bizarre Playlist"
   description = "Every song/artist referenced by JoJo's Bizarre Adventure. This playlist is managed by Terraform. To request updates or changes, please visit https://github.com/nnichols/jojos-bizarre-playlist"
